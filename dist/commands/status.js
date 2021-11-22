bf5fe9f5757af3afe3cab57a751a01c56c47909e
joel <joel33960@gmail.com>
2021-11-22 16:03:18 -0600

    catch

--- dist/commands/status.js
+++ dist/commands/status.js
@@ -14,7 +14,11 @@ exports.default = {
             msg.channel.messages.fetch().then((res) => {
                 const message = res.find((message) => message.embeds[0]?.title === `Id:\n${id}` && message.author.username === "Helix");
                 if (!message) {
-                    msg.channel.send(`The id "${id}" does not exist`).then((res) => msg.delete());
+                    msg.channel.send(`The id "${id}" does not exist`)
+                        .then((res) => msg.delete())
+                        .catch((err) => {
+                        console.log(err);
+                    });
                     return;
                 }
                 ;
