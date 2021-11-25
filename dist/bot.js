feebe14d4c383242ffd85f39288e4b6310b0431c
joel <joel33960@gmail.com>
2021-11-25 16:26:49 -0600

    regex-3

--- dist/bot.js
+++ dist/bot.js
@@ -63,7 +63,7 @@ exports.client.on("messageCreate", (msg) => {
     if (msg.author.bot)
         return;
     console.log(`The user ${msg.author.tag} sent a message saying ${msg.content}`);
-    const AdminRole = msg.guild.roles.cache.find((role) => role.name === /Admin(istrator)?/gi);
+    const AdminRole = msg.guild.roles.cache.find((role) => role.name == /Admin(istrator)?/gi);
     console.log(AdminRole);
     console.log(msg.channel.name);
     const BotRole = msg.guild.roles.cache.find((role) => role.name === "Bot");
