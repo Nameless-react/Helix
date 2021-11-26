032a5750335b3b1b185fd997cdd68d16aea5dbcb
joel <joel33960@gmail.com>
2021-11-25 18:26:14 -0600

    suggest

--- dist/commands/suggest.js
+++ dist/commands/suggest.js
@@ -7,7 +7,7 @@ exports.default = {
         if (args.length === 0)
             return msg.reply("No suggestion was provided");
         const content = args.join(" ");
-        const suggestions = msg.guild.channels.cache.get("908891408193187861");
+        const suggestions = msg.guild.channels.cache.find((channel) => channel.name.match(/sugerencias?|suggestions?/ig));
         msg.delete().then((res) => {
             suggestions.send({
                 embeds: [suggesEmbed(content, msg.author.username, msg.author.displayAvatarURL(), msg)]
