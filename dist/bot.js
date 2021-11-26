407a0ff9db6af33416a477a5962ae6f2c0a06a2b
joel <joel33960@gmail.com>
2021-11-25 18:59:22 -0600

    mute-5

--- dist/bot.js
+++ dist/bot.js
@@ -64,7 +64,6 @@ exports.client.on("messageCreate", (msg) => {
         return;
     console.log(`The user ${msg.author.tag} sent a message saying ${msg.content}`);
     const AdminRole = msg.guild.roles.cache.find((role) => role.name.match(/admins?(istrator)?|administrador/gi));
-    console.log(AdminRole);
     console.log(msg.channel.name);
     const BotRole = msg.guild.roles.cache.find((role) => role.name.match(/bots?|robots?|automaton/gi));
     const MainRole = msg.guild.roles.cache.find((role) => role.name.match(/members?|miembros?|normal|basic/ig));
