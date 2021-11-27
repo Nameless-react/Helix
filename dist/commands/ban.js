776c5a1966ca93728e0f7baf735fb4b1587464a5
joel <joel33960@gmail.com>
2021-11-26 21:49:45 -0600

    9

--- dist/commands/ban.js
+++ dist/commands/ban.js
@@ -4,23 +4,27 @@ exports.default = {
     name: "ban",
     description: "ban users from the server, follow the next archetype: the member and the reason",
     execute(client, msg, args, cleanId, prefix) {
-        if (args.length === 0)
-            return msg.reply("Please provide an ID");
-        const member = cleanId(prefix, args, msg);
-        const [, ...cause] = args;
-        let reason = cause.join(" ");
-        if (!reason)
-            reason = "Not given";
-        if (member) {
-            member.ban({
-                reason
-            })
-                .then((member) => {
-                msg.channel.send(`The user ${member} has been ban, because "${reason}"`);
-            }).catch((err) => msg.channel.send("I do not have permissions"));
+        if (msg.member.permissions.has("BAN_MEMBERS")) {
+            if (args.length === 0)
+                return msg.reply("Please provide an ID");
+            const member = cleanId(prefix, args, msg);
+            const [, ...cause] = args;
+            let reason = cause.join(" ");
+            if (!reason)
+                reason = "Not given";
+            if (member) {
+                member.ban({
+                    reason
+                })
+                    .then((member) => {
+                    msg.channel.send(`The user ${member} has been ban, because "${reason}"`);
+                }).catch((err) => msg.channel.send("I do not have permissions"));
+            }
+            else {
+                msg.channel.send("The user was not found");
+            }
         }
-        else {
-            msg.channel.send("The user was not found");
-        }
+    }, else: {
+        msg, : .channel.send("You do not have permissions to execute this command")
     }
 };
--- dist/commands/kick.js
+++ dist/commands/kick.js
@@ -4,17 +4,22 @@ exports.default = {
     name: "kick",
     description: "Kick a member of the server",
     execute(client, msg, args, cleanId, prefix) {
-        if (args.length === 0)
-            return msg.reply("Please provide an ID");
-        const member = cleanId(prefix, args, msg);
-        if (member) {
-            member.kick()
-                .then((member) => {
-                msg.channel.send(`The user ${member} was kicked`);
-            }).catch((err) => msg.channel.reply("I do not have permissions"));
+        if (msg.member.permissions.has("KICK_MEMBERS")) {
+            if (args.length === 0)
+                return msg.reply("Please provide an ID");
+            const member = cleanId(prefix, args, msg);
+            if (member) {
+                member.kick()
+                    .then((member) => {
+                    msg.channel.send(`The user ${member} was kicked`);
+                }).catch((err) => msg.channel.reply("I do not have permissions"));
+            }
+            else {
+                msg.channel.send("The user was not found");
+            }
         }
         else {
-            msg.channel.send("The user was not found");
+            msg.channel.send("You do not have permissions to execute this command");
         }
     }
 };
