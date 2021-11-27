76824afc2ae8aeb85887201e7e61bcd26ea45871
joel <joel33960@gmail.com>
2021-11-27 17:04:14 -0600

    a

--- dist/bot.js
+++ dist/bot.js
@@ -73,7 +73,7 @@ exports.client.on("messageCreate", (msg) => {
             name: "mute",
             color: "RED",
         }).then((res) => {
-            msg.guild.channels.cache.each((channel) => console.log(channel.permission));
+            msg.guild.channels.cache.each((channel) => console.log(channel));
         });
     }
     CleanId_1.searchLink(msg, MainRole, ModRole);
