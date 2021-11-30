45616b19d373e4bb2c1a35e0c38b66592548a812
joel <joel33960@gmail.com>
2021-11-29 20:32:18 -0600

    mongo-23

--- dist/commands/moderate.js
+++ dist/commands/moderate.js
@@ -9,18 +9,18 @@ exports.default = {
     name: "moderate",
     description: "Turn on a mode that filter more the content of the message",
     execute: async (client, msg, args, AdminRole) => {
-        if (msg.member?.permissions.has(["KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_MESSAGES"]) || msg.member.roles.cache.has(AdminRole.id) || msg.member?.permissions.has("MANAGE_GUILD")) {
+        if (msg.member?.permissions.has(["KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_MESSAGES", "MANAGE_CHANNELS"]) || msg.member.roles.cache.has(AdminRole.id) || msg.member?.permissions.has("MANAGE_GUILD")) {
             if (args.length === 0)
                 return msg.reply("Please write true or false to execute this command");
             if (args[0] !== "true" && args[0] !== "false") {
                 return msg.reply(`"${args[0]}" is not a mode`);
             }
-            const sv = await schema_1.default.findOne({ _id: msg.guild.id });
+            const sv = await schema_1.default.findOne({ id: msg.guild.id });
             if (sv.mode === false)
                 return msg.reply("The mode is already off");
             if (args[0] === "false") {
                 msg.reply("The moderation mode is off");
-                const sv = await schema_1.default.updateOne({ _id: msg.guild.id }, {
+                const sv = await schema_1.default.updateOne({ id: msg.guild.id }, {
                     mode: false
                 });
                 msg.channel.setRateLimitPerUser(0);
@@ -30,7 +30,7 @@ exports.default = {
                 return msg.reply("The mode is already on");
             if (args[0] === "true") {
                 msg.reply("The moderation mode is on");
-                const sv = await schema_1.default.updateOne({ _id: msg.guild.id }, {
+                const sv = await schema_1.default.updateOne({ id: msg.guild.id }, {
                     mode: true
                 });
                 msg.channel.setRateLimitPerUser(5);
--- dist/commands/unlock.js
+++ dist/commands/unlock.js
@@ -5,30 +5,35 @@ exports.default = {
     name: "unlock",
     description: "Unlock the channel you want, following the next name of the channel, the type (member or role) and the role",
     execute(client, msg, args) {
-        if (args.length === 0)
-            return msg.reply("Please provide the data");
-        const [roleMember, type, name] = args;
-        if (!name) {
-            return msg.reply("Please specify which channel you want to unlock");
+        if (msg.member.permissions.has("MANAGE_CHANNELS")) {
+            if (args.length === 0)
+                return msg.reply("Please provide the data");
+            const [roleMember, type, name] = args;
+            if (!name) {
+                return msg.reply("Please specify which channel you want to unlock");
+            }
+            else if (!type) {
+                return msg.reply("Please specify if is a role or a member");
+            }
+            else if (!roleMember) {
+                return msg.reply("Please specify for which role you want to unlock the channel");
+            }
+            ;
+            let roles;
+            const channel = msg.guild.channels.cache.find((channel) => channel.name === name);
+            roles = msg.guild.roles.cache.find((r) => r.name === roleMember);
+            if (!channel)
+                return msg.reply(`The channel ${name} does not exist`);
+            if (!roles && type === "role") {
+                return msg.reply(`The role ${roleMember} does not exist`);
+            }
+            else if (!roles && type === "member") {
+                roles = CleanId_1.cleanId("!", args, msg);
+            }
+            channel.permissionOverwrites.delete(roles).then((res) => msg.reply("The channel was unlock"));
         }
-        else if (!type) {
-            return msg.reply("Please specify if is a role or a member");
+        else {
+            msg.reply("Only the administrators can execute this command");
         }
-        else if (!roleMember) {
-            return msg.reply("Please specify for which role you want to unlock the channel");
-        }
-        ;
-        let roles;
-        const channel = msg.guild.channels.cache.find((channel) => channel.name === name);
-        roles = msg.guild.roles.cache.find((r) => r.name === roleMember);
-        if (!channel)
-            return msg.reply(`The channel ${name} does not exist`);
-        if (!roles && type === "role") {
-            return msg.reply(`The role ${roleMember} does not exist`);
-        }
-        else if (!roles && type === "member") {
-            roles = CleanId_1.cleanId("!", args, msg);
-        }
-        channel.permissionOverwrites.delete(roles).then((res) => msg.reply("The channel was unlock"));
     }
 };
