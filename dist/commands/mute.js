7eb4248fc88e9bc4331f7cacab4f69d45ab53b26
joel <joel33960@gmail.com>
2021-12-06 19:21:06 -0600

    mongo-125

--- dist/commands/mute.js
+++ dist/commands/mute.js
@@ -20,7 +20,7 @@ exports.default = {
             if (role && role.id !== MuteRole.id)
                 MainRole.push(role.id);
         });
-        console.log(memberMute.roles.cache.hasAny(sv.roles.main));
+        console.log(memberMute.roles.cache.hasAll(sv.roles.main));
         if (!memberMute.roles.cache.has(sv.roles.main))
             return;
         if (!memberMute.roles.cache.has(MuteRole.id)) {
