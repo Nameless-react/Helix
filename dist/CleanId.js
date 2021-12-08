e99b0ce4fe199a21a333c08749fd0d4e8bc33e90
joel <joel33960@gmail.com>
2021-12-07 22:00:12 -0600

    mongo-150

--- dist/CleanId.js
+++ dist/CleanId.js
@@ -146,8 +146,8 @@ const ProfileEmbed = (member) => {
     const string = roles.join(",");
     const embed = new discord_js_1.MessageEmbed()
         .setTitle("Profile:")
-        .setAuthor(`<@&${member.user.id}>`, member.user.displayAvatarURL())
-        .addFields({ name: "Id:", value: `${member.id}`, inline: true }, { name: "Roles:", value: `${string}`, inline: true }, { name: "Presence:", value: `${member.presence.activities}`, inline: true }, { name: "Time in guild:", value: `${member.joinedTimestamp / 1000 * 60 * 60 * 24}`, inline: true }, { name: "Status:", value: `${member.presence.status}`, inline: true })
+        .setAuthor(member.user.username, member.user.displayAvatarURL())
+        .addFields({ name: "Id:", value: `${member.user.id}`, inline: true }, { name: "Roles:", value: `${string}`, inline: true }, { name: "Presence:", value: `${member.presence.activities}`, inline: true }, { name: "Time in guild:", value: `${member.joinedTimestamp / 1000 * 60 * 60 * 24}`, inline: true }, { name: "Status:", value: `${member.presence.status}`, inline: true })
         .setColor("DARK_NAVY");
     return embed;
 };
