3386b824c2adf9cf8bee6159ed7373070a7ec0a4
joel <joel33960@gmail.com>
2021-12-14 17:08:53 -0600

    mongo-228

--- dist/commands/webhook.js
+++ dist/commands/webhook.js
@@ -1,6 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const CleanId_1 = require("../CleanId");
+const schema_1 = __importDefault(require("../schema"));
 exports.default = {
     name: "webHook",
     description: "Create a webhook, follow the next, the name of the channel you want the webhook to send the messages, the name of the webhook and if you want the image for(the link)",
@@ -11,8 +14,28 @@ exports.default = {
         const channel = msg.guild.channels.cache.find((channel) => channel.name === nameChannel);
         if (!channel)
             return msg.reply("The channel does not exist");
-        CleanId_1.WebHook.edit({
-            channel: channel.id
-        });
+        if (!img) {
+            channel.createWebhook(name)
+                .then(async (res) => {
+                const sv = await schema_1.default.updateOne({ id: String(msg.guild.id) }, {
+                    $set: {
+                        "webHook": res
+                    }
+                });
+                msg.reply("Webhook created");
+            });
+        }
+        else {
+            channel.createWebhook(name, {
+                avatar: img
+            }).then(async (res) => {
+                const sv = await schema_1.default.updateOne({ id: String(msg.guild.id) }, {
+                    $set: {
+                        "webHook": res
+                    }
+                });
+                msg.reply("Webhook created");
+            });
+        }
     }
 };
