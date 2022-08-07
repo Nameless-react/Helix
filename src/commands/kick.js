43e52f331ac8b60829bd42c84d9e1b091e4aea5c
joel <joel33960@gmail.com>
2022-08-06 21:52:54 -0600

    put away typescript

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,16 +1,16 @@
 export default {
     name: "kick",
     description: "Kick a member from the server",
-    execute(client:any, msg:any, args:string[]) {
+    execute(client, msg, args) {
         if (msg.member.permissions.has("KICK_MEMBERS")) {
             if (args.length === 0) return msg.reply("Please provide an ID");
             const { id } = msg.mentions.users.first();
             const member = msg.guild.members.cache.get(id);
             if (member) {
                 member.kick()
-                .then((member:any) => {
+                .then((member) => {
                     msg.channel.send(`The user ${member} was kicked`)
-                }).catch((err:any) => msg.reply("I do not have permissions"))
+                }).catch((err) => msg.reply("I do not have permissions"))
             } else {
                 msg.channel.send("The user was not found")
             }
